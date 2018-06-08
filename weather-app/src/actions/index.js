851faa2c1f3d83adca1fe9a993bedda40679b1b0
bipinkh <bipinkh91@gmail.com>
2018-06-08 20:21:34 +0545

    axios and react-promise

---
+++ weather-app/src/actions/index.js
@@ -0,0 +1,18 @@
+
+import FETCH_WEATHER from '../constants'
+import WEATHER_API_URL from '../constants'
+
+import axios from 'axios';
+
+//action creators to fetch weather using api requests
+ export function fetchWeather(city){
+   // for now, country is US.
+   const url = `${WEATHER_API_URL}&q=${city},us`;
+   const request = axios.get(url);
+
+   return{
+     type:FETCH_WEATHER
+     payload:request
+
+   }
+ }
