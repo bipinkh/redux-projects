bd00fde978f12f2106de49e9bb160b085ec6e397
bipinkh <bipinkh91@gmail.com>
2018-06-09 11:57:46 +0545

    Sparklines library used for charts

--- weather-app/src/actions/index.js
+++ weather-app/src/actions/index.js
@@ -7,7 +7,7 @@ import axios from 'axios';
 //action creators to fetch weather using api requests
  export function fetchWeather(city){
    // for now, country is US.
-   const url = `${WEATHER_API_URL}&q=${city},us`;
+   const url = `${WEATHER_API_URL}&q=${city},np`;
    const request = axios.get(url);  // request is promise
 
    console.log('Sent request ', request);
--- weather-app/src/containers/weather_list.js
+++ weather-app/src/containers/weather_list.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
+import Chart from '../components/chart'
 
 class WeatherList extends Component{
 
 
 renderWeather(cityData){
+  const name = cityData.city.name;
+  const temps = cityData.list.map( weather => weather.main.temp )
+  const pressure = cityData.list.map( weather => weather.main.pressure )
+  const humidity = cityData.list.map( weather => weather.main.humidity )
+  console.log(temps)
+
   return(
   <tr>
-    <td> {cityData.city.name} </td>
+    <td> {name} </td>
+    <td> <Chart data={temps} color="orange" /> </td>
+    <td> <Chart data={pressure} color="red" /> </td>
+    <td> <Chart data={humidity} color="green" /> </td>
   </tr>
-)
-  }
+  )
+}
 
 render(){
   console.log('city lists:' ,this.props.action_weather)
